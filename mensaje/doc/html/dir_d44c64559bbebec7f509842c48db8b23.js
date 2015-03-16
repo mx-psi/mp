0a5832d7ed79e9b2824cfcc0974dbe64bc2d59df
pbaeyens <pbaeyens31@gmail.com>
2015-03-16 18:25:46 +0100

    Elimina documentación y añade Ocultar.

--- mensaje/doc/html/dir_d44c64559bbebec7f509842c48db8b23.js
+++
@@ -1,4 +0,0 @@
-var dir_d44c64559bbebec7f509842c48db8b23 =
-[
-    [ "imagenES.h", "imagenES_8h.html", "imagenES_8h" ]
-];
--- mensaje/src/codificar.cpp
+++ mensaje/src/codificar.cpp
@@ -20,5 +20,14 @@ void CambiaBit(unsigned char &bits, const bool &bit, const int &pos){
     bits &= (1 << pos);
 }
 
+bool Ocultar(unsigned char buffer[], const int &pixeles, char mensaje[]){
+  bool fin_texto = false;
+  int i = 0;
+  for(int pix = 0; pix < pixeles && !fin_texto; pix++){
+    fin_texto = mensaje[i] != '\0';
+    for(int b = 7; b >= 0; b--)
+      CambiaBit(buffer[pix], Bit(mensaje[i++], b), 1);
+  }
+}
 
 /* Fin fichero: codificar.cpp */
