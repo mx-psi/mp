8923a2a638b3d2d96a1f0c30f1f5d0c72cd33d14
pbaeyens <pbaeyens31@gmail.com>
2015-03-17 13:26:57 +0100

    Arregla Makefile y quita TODOs.

--- mensaje/src/ocultar.cpp
+++ mensaje/src/ocultar.cpp
@@ -1,19 +1,12 @@
 /**
   * @file ocultar.cpp
   * @brief Fichero para codificar mensajes en imágenes.
-  *
-  * Incluye funciones para ocultar o revelar mensajes en imágenes
-  *
   */
 
 #include <iostream>
 #include "codificar.h"
 #include "imagenES.h"
 using namespace std;
-
-
-// TODO global: el ocultar.cpp anterior podría contener cosas interesantes implementables en este
-// otro TODO: "Incluye funciones para ocultar o revelar mensajes en imágenes" <-- wat?
 
 int main()
 {
@@ -22,25 +15,25 @@ int main()
    cout << "Introduzca la imagen de entrada: ";
    cin >> nombre;
    TipoImagen tipo = LeerTipoImagen(nombre, filas, columnas);
-   unsigned char imagen[filas*columnas*(1+2*(tipo == IMG_PPM))];     // TODO: que sea el triple si la imagen es en color pero de forma menos fea. Otro TODO: el estándar no deja usar vectores de longitud variable.
+   unsigned char imagen[filas*columnas*(1+2*(tipo == IMG_PPM))];
 
    if (tipo == IMG_PGM && LeerImagenPGM(nombre, filas, columnas, imagen)
     || tipo == IMG_PPM && LeerImagenPPM(nombre, filas, columnas, imagen))
    {
       char salida[256];
-      int bytes = filas*columnas*(1+2*(tipo == IMG_PPM))/8;   // TODO: ídem arriba. Quizá una variable las relacione. Otro TODO: el estándar no deja usar vectores de longitud variable.
+      int bytes = filas*columnas*(1+2*(tipo == IMG_PPM))/8;
       char mensaje[bytes];
       cout << "Introduzca la imagen de salida: ";
       cin >> salida;
       cout << "Introduzca el mensaje: ";
-      cin >> mensaje;   // TODO: Modificar para que acepte espacios
+      cin >> mensaje;
 
       cout << "Ocultando..." << endl;
       if (Ocultar(imagen, bytes, mensaje + '\0'))
          if (tipo == IMG_PGM)
-            EscribirImagenPGM(salida, imagen, filas, columnas);   // TODO: Debe añadirse .pgm en salida
+            EscribirImagenPGM(salida, imagen, filas, columnas);
          else
-            EscribirImagenPPM(salida, imagen, filas, columnas);   // TODO: ídem pero .ppm
+            EscribirImagenPPM(salida, imagen, filas, columnas);
       else
       {
          cout << "Error al intentar ocultar el mensaje.";
@@ -49,7 +42,7 @@ int main()
    }
    else
    {
-      cout << "Error: Imagen inválida." << endl;   // TODO: Convendría que dijese qué tipo de error se ha producido (lo indica en el guion)
+      cout << "Error: Imagen inválida." << endl;
       return 1;
    }
 }
