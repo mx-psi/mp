cecb0a75e47fbffd12a9c57baa60fb544e8b7b4d
pbaeyens <pbaeyens31@gmail.com>
2015-03-17 13:49:44 +0100

    Reestructura ocultar.cpp (#15).

--- mensaje/src/ocultar.cpp
+++ mensaje/src/ocultar.cpp
@@ -8,43 +8,62 @@
 #include "imagenES.h"
 using namespace std;
 
-int main()
+int Dimension(const TipoImagen &tipo, const int &filas, const int &columnas)
+{
+  return filas*columnas*(1+2*(tipo == IMG_PPM));
+}
+
+int Bytes(const TipoImagen &tipo, const int &filas, const int &columnas)
+{
+  return Dimension(tipo, filas, columnas)/8;
+}
+
+int main(int argc, char* argv[])
 {
    char nombre[256];
    int filas, columnas;
    cout << "Introduzca la imagen de entrada: ";
    cin >> nombre;
    TipoImagen tipo = LeerTipoImagen(nombre, filas, columnas);
-   unsigned char imagen[filas*columnas*(1+2*(tipo == IMG_PPM))];
+   unsigned char imagen[Dimension(tipo, filas, columnas)];
 
-   if (tipo == IMG_PGM && LeerImagenPGM(nombre, filas, columnas, imagen)
-    || tipo == IMG_PPM && LeerImagenPPM(nombre, filas, columnas, imagen))
-   {
-      char salida[256];
-      int bytes = filas*columnas*(1+2*(tipo == IMG_PPM))/8;
-      char mensaje[bytes];
-      cout << "Introduzca la imagen de salida: ";
-      cin >> salida;
-      cout << "Introduzca el mensaje: ";
-      cin >> mensaje;
-
-      cout << "Ocultando..." << endl;
-      if (Ocultar(imagen, bytes, mensaje + '\0'))
-         if (tipo == IMG_PGM)
-            EscribirImagenPGM(salida, imagen, filas, columnas);
-         else
-            EscribirImagenPPM(salida, imagen, filas, columnas);
-      else
-      {
-         cout << "Error al intentar ocultar el mensaje.";
-         return 1;
-      }
+   if(tipo == IMG_DESCONOCIDO){
+     cerr << argv[0] << ": Tipo de imagen desconocido";
+     return 1;
    }
+
+   if (tipo == IMG_PGM)
+     if(!LeerImagenPGM(nombre, filas, columnas, imagen))
+     {
+       cerr << argv[0] << ": Fallo en la lectura de la imagen.";
+       return 1;
+     }
    else
-   {
-      cout << "Error: Imagen inválida." << endl;
-      return 1;
-   }
+      if(!LeerImagenPPM(nombre, filas, columnas, imagen))
+      {
+        cerr << argv[0] << ": Fallo en la lectura de la imagen.";
+        return 1;
+      }
+
+    char salida[256];
+    int bytes = Bytes(tipo, filas, columnas);
+    char mensaje[bytes];
+    cout << "Introduzca la imagen de salida: ";
+    cin >> salida;
+    cout << "Introduzca el mensaje: ";
+    cin >> mensaje;
+
+    cout << "Ocultando..." << endl;
+    if (Ocultar(imagen, bytes, mensaje + '\0'))
+       if (tipo == IMG_PGM)
+          EscribirImagenPGM(salida, imagen, filas, columnas);
+       else
+          EscribirImagenPPM(salida, imagen, filas, columnas);
+    else
+    {
+       cout << "Error al intentar ocultar el mensaje.";
+       return 1;
+    }
 }
 
 /* Fin fichero: ocultar.cpp */
