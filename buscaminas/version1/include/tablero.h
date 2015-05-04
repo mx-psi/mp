5ea39b2f22c94076cc7257c52657d6e86a4758fd
josemam <jmlosvillares@correo.ugr.es>
2015-05-04 13:03:54 +0200

    tablero.h

---
+++ buscaminas/version1/include/tablero.h
@@ -0,0 +1,25 @@
+
+
+// Estructura con información para cada casilla
+struct Casilla
+{
+  bool bomba;
+  bool abierta;
+  bool marcada;
+
+  Casilla();  // TODO: Que no se me olvide definir este constructor. Que es muy olvidable.
+}
+
+
+// Clase Tablero y cabeceras
+class Tablero
+{
+
+public:
+  int Filas();
+  int Columnas();
+  
+
+
+
+};
