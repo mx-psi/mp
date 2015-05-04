bf6273fcc6d7313d29170db8ec1a6e8d7745f714
pbaeyens <pbaeyens31@gmail.com>
2015-05-04 13:39:03 +0200

    Arregla CampoMinas.cpp

--- buscaminas/version1/src/CampoMinas.cpp
+++ buscaminas/version1/src/CampoMinas.cpp
@@ -1,48 +1,47 @@
-#include "Tablero.h"
-using namespace std;
+#include "tablero.h"
 
-class CampoMinas
+
+CampoMinas::CampoMinas(int filas, int columnas, int minas):tab(filas, columnas){}
+
+int CampoMinas::Filas() const {return tab.Filas();}
+int CampoMinas::Columnas() const {return tab.Columnas();}
+
+bool CampoMinas::Explotado() const
 {
-  Tablero tab;
+  /* Comprueba si alguna mina ha explotado. */
+  for(int i = 0; i < Filas(); i++)
+    for(int j = 0; j < Columnas(); j++)
+      if(tab.Get(i,j).bomba && tab.Get(i,j).abierta)
+        return true;
 
-public:
-  CampoMinas(int filas, int columnas, int minas):tab(filas, columnas){}
+  return false;
+}
 
-  int Filas() {return tab.Filas();}
-  int Columnas() {return tab.Columnas();}
+bool CampoMinas::Ganado() const
+{
+  /* Indica si se ha ganado la partida. */
+  for(int i = 0; i < Filas(); i++)
+    for(int j = 0; j < Columnas(); j++)
+      if(!tab.Get(i,j).bomba && !tab.Get(i,j).abierta)
+        return false;
 
-  bool Explotado()
-  {
-    /* Comprueba si alguna mina ha explotado. */
-    for(int i = 0; i < Filas(); i++)
-      for(int j = 0; j < Columnas(); j++)
-        if(tab.Get(i,j).bomba && tab.Get(i,j).abierta)
-          return true;
+  return true;
+}
 
+bool CampoMinas::Marca(int x, int y)
+{
+  /* Marca o desmarca una casilla cerrada. Devuelve éxito. */
+
+  if(tab.Get(x,y).abierta) || x > tab.Filas()
+    || y > Tab.Columnas() || x < 0 || y < 0)
     return false;
-  }
 
-  bool Ganado()
-  {
-    /* Indica si se ha ganado la partida. */
-    for(int i = 0; i < Filas(); i++)
-      for(int j = 0; j < Columnas(); j++)
-        if(!tab.Get(i,j).bomba && !tab.Get(i,j).abierta)
-          return false;
+  Casilla nueva = tab.Get(x,y);
+  nueva.marcada = !nueva.marcada;
+  tab.Set(x,y,nueva);
+}
 
-    return true;
-  }
-
-  bool Marca(int x, int y)
-  {
-    /* Marca o desmarca una casilla cerrada. Devuelve éxito. */
-
-    if(tab.Get(x,y).abierta) || x > tab.Filas()
-      || y > Tab.Columnas() || x < 0 || y < 0)
-      return false;
-
-    Casilla nueva = tab.Get(x,y);
-    nueva.marcada = !nueva.marcada;
-    tab.Set(x,y,nueva);
-  }
-};
+void CampoMinas::PrettyPrint() const
+{
+  /* Imprime el estado actual del tablero. */
+}
