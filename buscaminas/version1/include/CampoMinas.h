a45d66a3c67f121b779c02b9b77ac8f75b3b7fc8
pbaeyens <pbaeyens31@gmail.com>
2015-05-04 13:05:33 +0200

    Corrige const

--- buscaminas/version1/include/CampoMinas.h
+++ buscaminas/version1/include/CampoMinas.h
@@ -4,12 +4,12 @@ class CampoMinas{
   Tablero tab;
 public:
   CampoMinas(int filas, int columnas, int minas);
-  const int Filas();
-  const int Columnas();
-  const bool Explotado();
-  const bool Ganado();
+  int Filas() const;
+  int Columnas() const;
+  bool Explotado() const;
+  bool Ganado() const;
   bool Marca(int x, int y);
   bool Abre(int x, int y);
-  void PrettyPrint();
-  void ImprimeTablero();
+  void PrettyPrint() const;
+  void ImprimeTablero() const;
 };
