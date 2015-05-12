b4cab6aa37fbb5e97e959a2563eacf574b36dfbc
pbaeyens <pbaeyens31@gmail.com>
2015-05-12 12:07:25 +0200

    version2: Sobrecarga << y >> Casilla

--- buscaminas/version2/src/Tablero.cpp
+++ buscaminas/version2/src/Tablero.cpp
@@ -19,3 +19,21 @@ void Tablero::Set(int fila, int columna, const Casilla &c)
 {
   datos[Columnas()*fila+columna] = c;
 }
+
+ostream& operator << (ostream& os, const Casilla& c)
+{
+  os << c.bomba;
+  os << c.abierta;
+  os << c.marcada;
+  return os;
+}
+
+istream& operator >> (istream& is, Casilla& c)
+{
+  while(isspace(is.peek()))
+    is.ignore();
+  c.bomba   = is.get() == '1' ? 1 : 0;
+  c.abierta = is.get() == '1' ? 1 : 0;
+  c.marcada = is.get() == '1' ? 1 : 0;
+  return is;
+}
