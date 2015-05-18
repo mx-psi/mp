b5618c8a0d77192e95989a9bdb3c3ea34a526e75
pbaeyens <pbaeyens31@gmail.com>
2015-05-18 13:03:48 +0200

    Sobrecargas

--- buscaminas/version2/src/Tablero.cpp
+++ buscaminas/version2/src/Tablero.cpp
@@ -25,24 +25,54 @@ std::ostream& operator << (std::ostream& os, const Casilla& c)
   os << c.bomba;
   os << c.abierta;
   os << c.marcada;
+  os << " ";
   return os;
 }
 
 std::istream& operator >> (std::istream& is, Casilla& c)
 {
-  while(isspace(is.peek())) //TODO: No sé si esto es necesario
+  char actual;
+
+  while(isspace(is.peek()))
     is.ignore();
-  c.bomba   = is.get() == '1' ? 1 : 0; //TODO: Debería fallar si es 'a' p.ej.?
-  c.abierta = is.get() == '1' ? 1 : 0; //TODO: Poner fallos
-  c.marcada = is.get() == '1' ? 1 : 0;
+
+  bool& campos[3] = {&c.bomba, &c.abierta, &c.marcada};
+
+  for(int i = 0; i < 3; i++)
+  {
+    actual = is.get();
+
+    if(actual != '1' || actual != '0')
+      is.setstate(std::ios_base::badbit);
+
+    campo[i] = actual == '1' ? 1 : 0;
+  }
+
   return is;
 }
 
 std::ostream& operator << (std::ostream& os, const Tablero& t)
 {
   os << t.Filas() << " " << t.Columnas() << endl;
+
   for(int i = 0; i < t.Filas(); i++)
     for(int j = 0; j < t.Columnas(); j++)
       os << t(i,j);
   return os;
+}
+
+std::istream& operator >> (std::istream& is, Tablero& t)
+{
+  int filas, columnas;
+  is >> filas >> columnas;
+  Tablero leido(filas, columnas);
+
+  for(int i = 0; i < leido.Filas(); i++)
+    for(int j = 0; j < leido.Columnas(); j++)
+      is >> leido(i,j);
+
+  if(is.good())
+    t = leido;
+
+  return is;
 }
